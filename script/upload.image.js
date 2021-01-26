d51e2ea2cc72a4ac6cb7c0efcccedf59bd66803b
cristtianjj <charlsmaritz@gmail.com>
2021-01-26 15:50:51 -0500

    vesion 3.0 carga efectivas de la imagen

--- script/upload.image.js
+++ script/upload.image.js
@@ -1,17 +1,33 @@
 const imgPreview = document.getElementById('img-preview');
 const input = document.getElementById('img-uploader');
+const imgVisible = document.querySelector('.edit-img');
+const deleteImg = document.getElementById('delete-img');
+const portraitImg = document.getElementById('portrait-img');
 
-input.addEventListener('change',(data) =>{
 
-    let reader = new FileReader();
-    const image = data.target.files[0]
-
-/*     if(!image){
+input.addEventListener('change',(data) =>{  
+    const image = data.target.files[0];
+    if(!image){
         imgPreview.src = '';
         return;
-    } */
+    }
 
+    let name = data.target.files[0].name
     const url = URL.createObjectURL(image);
-    imgPreview.src = url;
+    var width, height;
 
+    imgPreview.src = url;
+    portraitImg.src = url;
+    imgVisible.style.setProperty('visibility', 'visible');
+})
+
+imgPreview.onload = function() {
+    width  = imgPreview.naturalWidth  || imgPreview.width;
+    height = imgPreview.naturalHeight || imgPreview.height; 
+    console.log(width,height)
+}
+
+deleteImg.addEventListener('click', ()=>{
+    imgPreview.src = '';
+    imgVisible.style.setProperty('visibility', 'hidden');
 })
