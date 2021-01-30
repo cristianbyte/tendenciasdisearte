f07fbd60cd3eadef3d07e8a9265b156e6c266f87
cristtianjj <charlsmaritz@gmail.com>
2021-01-29 19:01:14 -0500

    progreso en la aplicacion de diseñp y previsuzlización de imagenes

--- script/upload.image.js
+++ script/upload.image.js
@@ -2,8 +2,6 @@ const imgPreview = document.getElementById('img-preview');
 const input = document.getElementById('img-uploader');
 const imgVisible = document.querySelector('.edit-img');
 const deleteImg = document.getElementById('delete-img');
-const portraitImg = document.getElementById('portrait-img');
-
 
 input.addEventListener('change',(data) =>{  
     const image = data.target.files[0];
@@ -17,7 +15,8 @@ input.addEventListener('change',(data) =>{
     var width, height;
 
     imgPreview.src = url;
-    portraitImg.src = url;
+    activeCnavas(url)
+
     imgVisible.style.setProperty('visibility', 'visible');
 })
 
@@ -31,3 +30,30 @@ deleteImg.addEventListener('click', ()=>{
     imgPreview.src = '';
     imgVisible.style.setProperty('visibility', 'hidden');
 })
+
+function activeCnavas(url) {
+    let canvas = document.getElementById('portrait-img');
+
+    let widthEditArea = (window.screen.width)*0.5
+    let heightEditArea = widthEditArea/16*9;
+
+    console.log(widthEditArea,heightEditArea)
+
+    let ctx = canvas.getContext('2d');
+
+    let image = new Image();
+
+    image.onload = function() {
+        ctx.drawImage(image, 400, 0, 200, image.naturalHeight,
+            100,0, 200, image.naturalHeight)
+        ctx.drawImage(image, 700, 0, 200, image.naturalHeight,
+            400,0, 200, image.naturalHeight)
+    }
+
+    canvas.style.setProperty('width', widthEditArea + 'px')
+    canvas.style.setProperty('height', heightEditArea + 'px')
+
+    image.src = url;
+    console.log(url)
+
+}
