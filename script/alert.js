39776c527c9f3c1608b05d5ee58308ea6f66ddc2
cristtianjj <charlsmaritz@gmail.com>
2021-02-01 20:23:23 -0500

    ajustes, revision de bugs, y detalles UI

---
+++ script/alert.js
@@ -0,0 +1,36 @@
+const newAlert = document.getElementById('alert')
+
+function callingAlert(icon = 0, message = 'Something is wrong' ){
+
+    const routeSvgAlert = ['check.svg', 'warning.svg', 'error.svg']
+
+    switch (icon) {
+        case 0:
+            newAlert.style.setProperty('background-color', 'green')
+            break
+        case 1:
+            newAlert.style.setProperty('background-color', '#f58634')
+            break
+        case 2: 
+            newAlert.style.setProperty('background-color', 'red')
+            break
+    }
+
+    newAlert.innerHTML = `<img class="alert__icon"  src="svg/${routeSvgAlert[icon]}" alt="Warning" srcset=""> <p class="alert__message">${message}</p>`
+    newAlert.style.setProperty('animation', 'rebote .4s ease forwards')
+    newAlert.style.setProperty('visibility', 'visible')
+
+    setTimeout(()=>{ goodByeAlert()}, 4000)
+}
+
+function goodByeAlert(){
+    newAlert.style.setProperty('visibility', 'hidden')
+    newAlert.style.setProperty('animation', 'unset')
+}
+
+newAlert.addEventListener('click', ()=>{
+    goodByeAlert()
+})
+
+/* visibility: visible;
+    animation: rebote .4s ease forwards; */
--- script/upload.image.js
+++ script/upload.image.js
@@ -1,59 +1,92 @@
-const imgPreview = document.getElementById('img-preview');
-const input = document.getElementById('img-uploader');
-const imgVisible = document.querySelector('.edit-img');
-const deleteImg = document.getElementById('delete-img');
+let littleImage = document.getElementById('img-preview');
+const buttonUpload = document.getElementById('img-uploader');
+const littleImageVisible = document.querySelector('.edit-img');
+const deleteButton = document.getElementById('delete-img');
+const canvasWriter = document.getElementById('preview-area')
+let url = undefined
+let imageExists = false
 
-input.addEventListener('change',(data) =>{  
+buttonUpload.addEventListener('change',(data) =>{  
     const image = data.target.files[0];
+
     if(!image){
-        imgPreview.src = '';
+        callingAlert(1,'¡Ops!, no encontre la foto.')
+        littleImageVisible.style.setProperty('visibility', 'hidden');
+        imageExists = false
         return;
     }
 
-    let name = data.target.files[0].name
-    const url = URL.createObjectURL(image);
-    var width, height;
+    /* let name = data.target.files[0].name */
+    url = URL.createObjectURL(image);
 
-    imgPreview.src = url;
-    activeCnavas(url)
+    littleImage.src = url
+    callingAlert(0,'¡Empecemos!')
+    imageExists = true
+    littleImageVisible.style.setProperty('visibility', 'visible');
 
-    imgVisible.style.setProperty('visibility', 'visible');
 })
 
-imgPreview.onload = function() {
-    width  = imgPreview.naturalWidth  || imgPreview.width;
-    height = imgPreview.naturalHeight || imgPreview.height; 
-    console.log(width,height)
-}
+editAreaSize.addEventListener('click', ()=>{
 
-deleteImg.addEventListener('click', ()=>{
-    imgPreview.src = '';
-    imgVisible.style.setProperty('visibility', 'hidden');
-})
-
-function activeCnavas(url) {
-    let canvas = document.getElementById('portrait-img');
-
-    let widthEditArea = (window.screen.width)*0.5
-    let heightEditArea = widthEditArea/16*9;
-
-    console.log(widthEditArea,heightEditArea)
-
-    let ctx = canvas.getContext('2d');
-
-    let image = new Image();
-
-    image.onload = function() {
-        ctx.drawImage(image, 400, 0, 200, image.naturalHeight,
-            100,0, 200, image.naturalHeight)
-        ctx.drawImage(image, 700, 0, 200, image.naturalHeight,
-            400,0, 200, image.naturalHeight)
+    var radVal = editAreaSize.rads.value;
+    if(!url || imageExists == false){
+        callingAlert(1,'Mejor sube una foto primero.')
+        for(let i =0 ;i< editAreaSize.length;i++){
+            editAreaSize[i].checked = false;
+        }
     }
+    else{
+        switch (radVal){
+            case '23x50':{
+                loadBigImage(url, 23, 50 )
+                break
+            }
+            case '35x40':{
+                loadBigImage(url, 35, 50 )
+                break
+            }
+            case '40x70':{
+                loadBigImage(url, 40, 70 )
+                break
+            }
+            case '50x70':{
+                loadBigImage(url, 50, 70 )
+                break
+            }
+        }
+    }
+    
 
-    canvas.style.setProperty('width', widthEditArea + 'px')
-    canvas.style.setProperty('height', heightEditArea + 'px')
+})
 
+function loadBigImage(url, widthSize, heightSize ) {
+    
+    let heightEditArea = ((window.screen.height*0.6)/70)*heightSize;
+    let widthEditArea = (heightEditArea/heightSize)*widthSize
+    widthEditArea= Math.trunc(widthEditArea)
+
+    canvasWriter.innerHTML = `<canvas id="portrait-img" width="${widthEditArea}px" height="${heightEditArea}px" > ¡Ops!, your browser does not support the canvas tag.</canvas>`;
+
+    let bigImage = document.getElementById('portrait-img');
+    let ctx = bigImage.getContext('2d');
+    
+    var image = new Image();
+    
+    image.onload = function() {
+        // (image,  pos_x_image,        pos_y_image,            width_image,    heigth_image, 
+        //          pos_x_to_start_draw, pos_y_to_start_draw,   width_draw,     height_draw)
+        
+        let height = littleImage.naturalHeight
+        let width  = (height/heightSize)*widthSize;
+        let center = (littleImage.naturalWidth-width)/2
+
+        ctx.drawImage(image,center,0,width,height,0,0,widthEditArea,heightEditArea)
+    }
     image.src = url;
-    console.log(url)
-
 }
+
+deleteButton.addEventListener('click', ()=>{
+    imageExists = false
+    littleImageVisible.style.setProperty('visibility', 'hidden');
+})
+
