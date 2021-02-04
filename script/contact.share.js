7bbb12dd497c487b28ab6553d69299a23f6496eb
cristtianjj <charlsmaritz@gmail.com>
2021-02-04 14:31:59 -0500

    update 2.2

---
+++ script/contact.share.js
@@ -0,0 +1,55 @@
+const contact = document.getElementById('contact')
+let messaje = undefined
+let turn = true
+let actual = window.location.href
+let includeShare = true
+actual.includes('design') ? includeShare = false : includeShare = true
+includeShare ? contact.style.setProperty('right','5%') : 
+
+console.log(includeShare)
+contact.addEventListener('click', ()=>{
+    if(includeShare){
+        if (turn) {
+            window.open('https://wa.me/573137673755?text=¡Hola!, necesito más informacion.')
+        } else {
+            window.open('https://instagram.com/tendenciasdiseart')
+        }
+    }
+    else if (turn) {
+        if(radVal !== undefined){
+            messaje = '¡Hola!,necesito más informacion sobre este cuadro ' + radVal
+            window.open('https://wa.me/573137673755?text=' + messaje)
+        }
+        else{
+            messaje = '¡Hola!, necesito una asesoria.'
+            window.open('https://wa.me/573137673755?text=' + messaje)
+        }
+    } else {
+        window.open('https://t.me/cristtianjj')
+    }
+})
+
+setInterval(()=>{
+    if(includeShare){
+        if(turn){
+            contact.innerHTML = '<img class="contact__icon" src="svg/instagram.svg" alt="contactInstagram" srcset="">'
+            turn = false
+        }
+        else{
+            contact.innerHTML = '<img class="contact__icon" src="svg/wp.svg" alt="contactWhatsapp" srcset="">'
+            turn = true
+        }
+    }
+    else{
+        if(turn){
+            contact.innerHTML = '<img class="contact__icon" src="svg/telegram.svg" alt="contactTelegram" srcset="">'
+            turn = false
+        }
+        else{
+            contact.innerHTML = '<img class="contact__icon" src="svg/wp.svg" alt="contactWhatsapp" srcset="">'
+            turn = true
+        }
+    }
+
+},5000);
+
