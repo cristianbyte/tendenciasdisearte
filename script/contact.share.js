6ec5f98f2fd58dcf4c465ed79858dc8eb4488727
cristtianjj <charlsmaritz@gmail.com>
2021-02-05 12:52:32 -0500

    update 2.1 shares

--- script/contact.share.js
+++ script/contact.share.js
@@ -15,40 +15,32 @@ contact.addEventListener('click', ()=>{
             window.open('https://www.instagram.com/tendenciasdisearte/')
         }
     }
-    else if (turn) {
-        if(radVal !== undefined){
-            messaje = '¡Hola!,necesito más informacion sobre este cuadro ' + radVal
-            window.open('https://wa.me/573024627133?text=' + messaje)
+    else{
+        if (turn) {
+            if(radVal !== undefined){
+                messaje = '¡Hola!,necesito más informacion sobre este cuadro ' + radVal
+                window.open('https://wa.me/573024627133?text=' + messaje)
+            }
+            else{
+                messaje = '¡Hola!, necesito una asesoria.'
+                window.open('https://wa.me/573024627133?text=' + messaje)
+            }
+        } else {
+            window.open('https://www.instagram.com/tendenciasdisearte/')
         }
-        else{
-            messaje = '¡Hola!, necesito una asesoria.'
-            window.open('https://wa.me/573024627133?text=' + messaje)
-        }
-    } else {
-        window.open('https://t.me/')
+
+
     }
 })
 
 setInterval(()=>{
-    if(includeShare){
-        if(turn){
-            contact.innerHTML = '<img class="contact__icon" src="svg/instagram.svg" alt="contactInstagram" srcset="">'
-            turn = false
-        }
-        else{
-            contact.innerHTML = '<img class="contact__icon" src="svg/wp.svg" alt="contactWhatsapp" srcset="">'
-            turn = true
-        }
+    if(turn){
+        contact.innerHTML = '<img class="contact__icon" src="svg/instagram.svg" alt="contactInstagram" srcset="">'
+        turn = false
     }
     else{
-        if(turn){
-            contact.innerHTML = '<img class="contact__icon" src="svg/telegram.svg" alt="contactTelegram" srcset="">'
-            turn = false
-        }
-        else{
-            contact.innerHTML = '<img class="contact__icon" src="svg/wp.svg" alt="contactWhatsapp" srcset="">'
-            turn = true
-        }
+        contact.innerHTML = '<img class="contact__icon" src="svg/wp.svg" alt="contactWhatsapp" srcset="">'
+        turn = true
     }
 
 },5000);
